5d7ba9825489b5af0424d54d24fcc60f648ee2ad
divyansh25bai10716 <divyansh.25bai10716@vitbhopal.ac.in>
2026-03-29 16:02:17 +0530

    Add files via upload

---
+++ error_fix.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+data = pd.read_csv("error_messages_explained.csv")
+error_list = data["error_message"].astype(str).tolist()
+explanation_list = data["explanation"].astype(str).tolist()
+
+def explain_error(user_error):
+
+    for i in range(len(error_list)):
+        
+        if error_list[i].lower() in user_error.lower():
+            return explanation_list[i]
+
+    return "New error detected — explanation not available."
+
+print("Python Error Explainer")
+
+while True:
+    
+    error = input("\nEnter error: ")
+
+    if error.lower() == "quit":
+        break
+
+    print("\nExplanation:")
+    print(explain_error(error))
