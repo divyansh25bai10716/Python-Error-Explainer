d1fe2b8db5c540dbde226e8a9b52c2fee4ffdd41
divyansh25bai10716 <divyansh.25bai10716@vitbhopal.ac.in>
2026-03-30 10:22:59 +0530

    error_fix.py

--- error_fix.py
+++ error_fix.py
@@ -1,23 +1,40 @@
 import pandas as pd
 
-data = pd.read_csv("error_messages_explained.csv")
+csv_file = "error_messages_explained.csv"
+
+data = pd.read_csv(csv_file)
+
 error_list = data["error_message"].astype(str).tolist()
 explanation_list = data["explanation"].astype(str).tolist()
+
 
 def explain_error(user_error):
 
     for i in range(len(error_list)):
-        
         if error_list[i].lower() in user_error.lower():
             return explanation_list[i]
 
-    return "New error detected — explanation not available."
+    print("\nNew error detected!")
+    user_explanation = input("Please enter explanation for this error: ")
+
+    new_data = pd.DataFrame({
+        "error_message": [user_error],
+        "explanation": [user_explanation]
+    })
+
+    new_data.to_csv(csv_file, mode='a', header=False, index=False)
+
+    error_list.append(user_error)
+    explanation_list.append(user_explanation)
+
+    return "New error saved successfully!"
+
 
 print("Python Error Explainer")
-print("type QUIT if no error")
+print("Type QUIT if no error")
 
 while True:
-    
+
     error = input("\nEnter error: ")
 
     if error.lower() == "quit":
